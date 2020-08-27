ec85d81a1ab6229c6f56e74dcde87cfcdf1eb5c8
orkoren2006 <orkoren2006@gmail.com>
2020-08-27 23:16:34 +0300

    upgraded css 23:15

--- cmps/email-app/EmailPreview.jsx
+++ cmps/email-app/EmailPreview.jsx
@@ -7,10 +7,12 @@ export function EmailPreview (props) {
         <li className={read} onClick={(ev)=> {props.onReadEmail(props.email.id, ev)}}>
             <img src="../../assets/img/delete.png" name="delete" onClick={()=> {props.onRemoveEmail(props.email.id)}}/>
             <img src={`../../assets/img/${star}.png`} name="starred" onClick={()=> {props.onStarredEmail(props.email.id)}}/>
-            <h2>{props.email.sender}</h2>
-            <h2>{props.email.subject}</h2>
-            <h3>{props.email.body.substring(0, 50)}</h3>
+            <p className="sender">{props.email.sender}</p>
+            <p className="subject">{props.email.subject}</p>
+            <p className="email-body">{props.email.body.substring(0, 50)}</p>
+            <p className="date">Aug 27</p>
             {/* <h3>{email.sentAt}</h3> */}
         </li>
+        
     )
 }
