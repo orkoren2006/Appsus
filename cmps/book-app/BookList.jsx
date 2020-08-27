355cf90f4068a6a8647091f505698030a5743d53
ItayAvrahami <itayavrahami1@gmail.com>
2020-08-28 00:30:05 +0300

    good night

--- cmps/book-app/BookList.jsx
+++ cmps/book-app/BookList.jsx
@@ -1,14 +1,17 @@
 import { BookPreview } from './BookPreview.jsx'
+const { Link } = ReactRouterDOM
 // import { GoogleBookPreview } from './GoogleBookPreview.jsx'
 
 
-export function BookList(props){
+export function BookList(props) {
 
     return (<ul className="book-list clean-list grid">
         {props.books.map(book => {
-            return <li key={book.id}>
-                <BookPreview book={book}/>
-            </li>
+            return <Link className="book-link" key={book.id} to={`/book/:${book.id}`}>
+                <li className="book-item" >
+                    <BookPreview book={book} />
+                </li>
+            </Link>
         })}
     </ul>)
 }
--- cmps/book-app/BookPreview.jsx
+++ cmps/book-app/BookPreview.jsx
@@ -1,16 +1,18 @@
-const { Link } = ReactRouterDOM
-import {utilService} from '../../services/utils.js'
+// const { Link } = ReactRouterDOM
+import { utilService } from '../../services/utils.js'
 
 export function BookPreview({ book }) {
     const priceToDisplay = utilService.getPrice(book.listPrice.amount, book.listPrice.currencyCode)
 
     return (
-        <Link to={`/book/${book.id}`}>
-            <article className="book-preview">
-                <img className="book-img" src={book.thumbnail} alt="" />
+        // <Link to={`/book/${book.id}`}>
+        <article className="book-preview">
+            <img className="book-img" src={book.thumbnail} alt="" />
+            <section className="book-text flex-col">
                 <h3>{book.title}</h3>
                 <h4>Price: {priceToDisplay}</h4>
-            </article>
-        </Link>
+            </section>
+        </article>
+        // </Link>
     )
 }
