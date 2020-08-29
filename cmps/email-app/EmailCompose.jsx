3796fe003692dd89f8f96ac9696df847091888e2
orkoren2006 <orkoren2006@gmail.com>
2020-08-29 22:26:42 +0300

    22:26

--- cmps/email-app/EmailCompose.jsx
+++ cmps/email-app/EmailCompose.jsx
@@ -2,20 +2,21 @@
 export function EmailCompose(props) {
     return (
         <section className={props.className}>
+            <button className="x-btn" onClick={props.closeModal}>X</button>
             <h3>New Message</h3>
-            <button onClick={props.closeModal}>X</button>
+            
             {/* <input type="text" placeholder="Enter Subject" onChange={(ev) => {
                 props.showSubject(ev.target.value)
             }} /> */}
              <textarea className="subject-input" onChange={(ev) => {
                 props.showBody(ev.target.value)
-            }} rows="1" cols="40" name="comment" form="usrform" placeholder="Subject"></textarea>
+            }} rows="1" cols="60" name="comment" form="usrform" placeholder="Subject"></textarea>
             {/* <input type="text" placeholder="Enter Message" onChange={(ev) => {
                 props.showBody(ev.target.value)
             }} /> */}
             <textarea className="message-input" onChange={(ev) => {
                 props.showBody(ev.target.value)
-            }} rows="20" cols="40" name="comment" form="usrform"></textarea>
+            }} rows="20" cols="60" name="comment" form="usrform"></textarea>
             <button className="add-note" onClick={props.sendEmail}>Send</button>
           
         </section>
