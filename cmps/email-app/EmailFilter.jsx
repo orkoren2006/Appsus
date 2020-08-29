daf4a2db099ec35df1ae2da39f1dcc2cca2adab1
orkoren2006 <orkoren2006@gmail.com>
2020-08-29 10:52:14 +0300

    10:52

--- cmps/email-app/EmailFilter.jsx
+++ cmps/email-app/EmailFilter.jsx
@@ -1,11 +1,22 @@
 
 
 export function EmailFilter (props) {
-    return <section className="filter-buttons">
-        <button onClick={props.onInbox}>Inbox</button>
-        <button onClick={props.onSentFilter}>Sent</button>
-        <button onClick={props.onStarredFilter}>Starred</button>
-        <button onClick={props.onReadFilter}>Read</button>
+    return (<section className="filter-buttons">
+        <div className="btn-container">
+        <img  className="icon-btn" src="../../assets/img/inbox-icon.png"/>
+        <button onClick={props.onInbox}>Inbox</button> </div>
+        <div className="btn-container">
+        <img  className="icon-btn" src="../../assets/img/send.png"/>
+        <button onClick={props.onSentFilter}>Sent</button></div>
+        <div className="btn-container">
+        <img  className="icon-btn" src="../../assets/img/star-grey.png"/>
+        <button onClick={props.onStarredFilter}>Starred</button></div>
+        <div className="btn-container">
+        <img  className="icon-btn" src="../../assets/img/read.png"/>
+        <button onClick={props.onReadFilter}>Read</button></div>
     </section>
-  
+    )
 }
+
+
+
